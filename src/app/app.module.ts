9c7b42349926d11f1f1caad4a0608edffe35398a
nauranaufal <cdapuribu@gmail.com>
2020-08-09 02:44:50 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+import { PostProvider } from '../providers/post-provider';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { HttpModule } from '@angular/http';
 
 @NgModule({
   declarations: [
@@ -14,6 +16,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +27,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    PostProvider,
+    InAppBrowser ,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,33 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { PostProvider } from '../../providers/post-provider';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-
-  constructor(public navCtrl: NavController) {
+  hasil:any=[];
+  order_id:any;
+  gross_amount:any;
+  constructor(
+    public navCtrl: NavController,
+    private iab: InAppBrowser,
+    private postPvdr : PostProvider) {
 
   }
 
+  prosesBeliMidtrans(){
+    let body = {
+      order_id:this.order_id,
+      gross_amount:this.gross_amount
+    }
+
+    this.postPvdr.postData(body, 'veritrans_checkout.php').subscribe((data:any) => {
+      console.log(data);
+      this.hasil=data;
+      this.iab.create(data.redirect_url);
+    });
+  }
 }
